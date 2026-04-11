c6b807b160dd9bf235ed3cd678a8e6d36392cd36
tps <tps@Ubuntu.myguest.virtualbox.org>
2026-04-11 19:49:09 -0400

    including slab.h and function prototypes to appease yocto

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -33,5 +33,12 @@ struct aesd_dev
     struct cdev cdev;     /* Char device structure      */
 };
 
+int aesd_open(struct inode *inode, struct file *filp);
+int aesd_release(struct inode *inode, struct file *filp);
+ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+static int aesd_setup_cdev(struct aesd_dev *dev);
+int aesd_init_module(void);
+void aesd_cleanup_module(void);
 
 #endif /* AESD_CHAR_DRIVER_AESDCHAR_H_ */
--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -17,6 +17,7 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
+#include <linux/slab.h>
 #include "aesdchar.h"
 
 int aesd_major =   0; // use dynamic major
